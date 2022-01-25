52da52a22b8e52a2e19ae431e2920d33d54f9a96
mohanad-alkrunz <muhannd@ahdtech.com>
2022-01-25 18:50:50 +0200

    Finish Contracts

--- me_group/api/contract.py
+++ me_group/api/contract.py
@@ -16,6 +16,27 @@ import json
 
 
 @frappe.whitelist(allow_guest=True)
+def get_conditions():
+    lang = "ar"
+    if frappe.get_request_header("Language"):
+        lang = frappe.get_request_header("Language")
+    frappe.local.lang = lang
+
+    result = []
+
+    conditions = frappe.get_all("Contract Condition",fields =["name","condition"],filters={"enabled":1})
+
+    for condition in conditions:
+        result.append({
+            "id":condition.name,
+            "condition":condition.condition
+        })
+
+    frappe.local.response['status'] = {"message": _("Conditions list"), "success": True, "code": 200}
+    frappe.local.response['data'] = result
+
+
+@frappe.whitelist(allow_guest=True)
 def create_contract(**kwards):
     lang = "ar"
     if frappe.get_request_header("Language"):
@@ -85,6 +106,8 @@ def create_contract(**kwards):
         license_image=license_image,
         instrument_image=instrument_image,
         diagrams=diagrams,
+        engineer_contact=engineer_contact,
+        instrument_no=instrument_no,
         docstatus=0,
     )).insert(ignore_permissions=True)
     contract.save(ignore_permissions=True)
@@ -92,12 +115,6 @@ def create_contract(**kwards):
 
     frappe.local.response['status'] = {"message": _("Contract Created Successfully"), "success": True, "code": 200}
     frappe.local.response['data'] = None
-
-
-
-
-
-
 
 
 @frappe.whitelist(allow_guest=True)
@@ -184,6 +201,98 @@ def get_contract_status(**kwards):
         },
 
     }
+
+@frappe.whitelist(allow_guest=True)
+def get_contract_details(**kwards):
+    lang = "ar"
+    if frappe.get_request_header("Language"):
+        lang = frappe.get_request_header("Language")
+    frappe.local.lang = lang
+    data = kwards
+    id = None
+    if "id" in data:
+        id = data['id']
+    else:
+        frappe.local.response['status'] = {"message": _("ID Required"), "success": False, "code": 403}
+        frappe.local.response['data'] = None
+
+    check = check_token()
+    user1 = None
+    if check and "user" in check:
+        user1 = check['user']
+
+    contract = frappe.get_doc("Contract Application",id)
+    conditions = []
+
+    for condition in contract.conditions:
+        conditions.append({
+            "condition":condition.condition_details,
+            "check":_(condition.base_select)
+        })
+
+
+
+    frappe.local.response['status'] = {"message": _("Contract Details"), "success": True, "code": 200}
+
+    frappe.local.response['data'] = {
+            "id" :contract.name,
+            "ID_no" :contract.id_no,
+            "id_release_date":contract.id_release_date,
+            "id_issuer":contract.id_issuer,
+            "id_image":contract.id_image,
+        "instrument_no":contract.instrument_no,
+        "instrument_date":contract.instrument_date,
+        "instrument_image":contract.instrument_image,
+        "license_no":contract.license_no,
+        "license_date":contract.license_date,
+        "license_image":contract.license_image,
+        "office_name":contract.office_name,
+        "engineer_name":contract.engineer_name,
+        "engineer_contact":contract.engineer_contact,
+        "diagrams":contract.diagrams,
+        "details":contract.details,
+        "customer_response":_(contract.customer_response),
+        "customer_note":contract.customer_note,
+        "price" : contract.price,
+        "conditions":conditions
+    }
+
+
+@frappe.whitelist(allow_guest=True)
+def update_customer_status(**kwards):
+    lang = "ar"
+    if frappe.get_request_header("Language"):
+        lang = frappe.get_request_header("Language")
+    frappe.local.lang = lang
+    data = kwards
+    id = None
+    response = "Agree"
+    note = ""
+
+    if "response" in data:
+        response = data['response']
+    if "note" in data:
+        note = data['note']
+
+    if "id" in data:
+        id = data['id']
+    else:
+        frappe.local.response['status'] = {"message": _("ID Required"), "success": False, "code": 403}
+        frappe.local.response['data'] = None
+
+    check = check_token()
+    user1 = None
+    if check and "user" in check:
+        user1 = check['user']
+
+    # contract = frappe.get_doc("Contract Application",id)
+    frappe.db.sql(f"""Update `tabContract Application` set customer_response ='{response}',customer_note ='{note}' where name='{id}'""")
+    frappe.db.commit()
+
+    frappe.local.response['status'] = {"message": _("contract updated successful"), "success": True, "code": 200}
+
+    frappe.local.response['data'] = None
+
 
 @frappe.whitelist(allow_guest=True)
 def update_contract_financial(**kwards):
