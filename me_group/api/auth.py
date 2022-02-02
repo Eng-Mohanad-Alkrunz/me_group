3f0a05ee1a60d9b52b579cc77c802a20de99f2a1
Eng-Mohanad-Alkrunz <muhannd@ahdtech.com>
2022-02-02 18:26:27 +0200

    Fix

--- me_group/api/auth.py
+++ me_group/api/auth.py
@@ -83,7 +83,7 @@ def login(**kwards):
             "full_name": full_name,
             "name": name
         }}
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     customer_devices = frappe.get_all("User Device", ['name'], filters={"udid": udid, "docstatus": ['<', 2]})
     customer_device = None
     if customer_devices:
@@ -227,7 +227,7 @@ def register(**kwards):
             "full_name": full_name,
             "name": name
         }}
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     # token = token.decode("utf-8")
 
     customer_devices = frappe.get_all("User Device", ['name'], filters={"udid": udid, "docstatus": ['<', 2]})
@@ -333,7 +333,7 @@ def change_password(**kwards):
             "full_name": customer.customer_name,
             "name": name
         }}
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     # token = token.decode("utf-8")
 
     current_token = frappe.get_request_header("Authorization").replace('Bearer ', '')
--- me_group/supervisor_api/auth.py
+++ me_group/supervisor_api/auth.py
@@ -84,7 +84,7 @@ def login(**kwards):
             "full_name": full_name,
             "name": name
         }};
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     # token = token.decode('utf-8')
     supervisor_devices = frappe.get_all("User Device", ['name'], filters={"udid": udid, "docstatus": ['<', 2]})
     supervisor_device = None
@@ -194,7 +194,7 @@ def change_password(**kwards):
             "full_name": supervisor.supervisor,
             "name": name
         }}
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     # token = token.decode('utf-8')
 
     current_token = frappe.get_request_header("Authorization").replace('Bearer ', '')
