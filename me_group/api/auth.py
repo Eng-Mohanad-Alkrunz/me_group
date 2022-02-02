0a2825c8f639c4a27bb06b83a1817b688034d789
Eng-Mohanad-Alkrunz <muhannd@ahdtech.com>
2022-02-02 19:32:23 +0200

    Test

--- me_group/api/auth.py
+++ me_group/api/auth.py
@@ -83,7 +83,7 @@ def login(**kwards):
             "full_name": full_name,
             "name": name
         }}
-    token = jwt.encode(token, secret_key, algorithm="HS256").decode("utf-8")
+    token = jwt.encode(token, secret_key, algorithm="HS256")
     customer_devices = frappe.get_all("User Device", ['name'], filters={"udid": udid, "docstatus": ['<', 2]})
     customer_device = None
     if customer_devices:
